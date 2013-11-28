f04bd92e28d0f98d168b17eb1b4951578482530d
lhyqie <lhymvp@gmail.com>
2013-11-28 16:23:18 -0600

    finished learning of CRF weights
    original crf.py saved as crf_origin.py
    crf_test.py is just a test
    main.py train() my part finished
    crf.py modified to accord with main
    changed cal feature function and each cal helper function accordingly

--- crf_test.py
+++ crf_test.py
@@ -1,16 +1,35 @@
+from numpy import genfromtxt
+from numpy import vstack
 from collections import defaultdict
 from math import log, exp
 import sys
 import operator
- 
+
 # The L2 regularization coefficient and learning rate for SGD
 l2_coeff = 1
-rate = 10
- 
-# A dictionary to map tags to integers
-tagids = defaultdict(lambda: len(tagids))
-tagids["<S>"] = 0
- 
+rate = 1
+
+
+A = genfromtxt('quiztrain.csv', delimiter=',', skip_header = 0)
+B = genfromtxt('quiztest.csv', delimiter=',', skip_header = 0)
+BX = B[0:10,:]
+BY = B[10:,:]
+
+XY = [
+         {12:[x for x in range(1, 11)]},
+         {13:[x for x in range(1, 11)]},
+         {20:[x for x in range(1, 11)]},
+         {16:[5,9], 15:[2,3], 14:[2,4]},
+         {},
+     ]
+YY = [
+        [12],
+        [13],
+        [20],
+        [16,15,14],
+        [11,17,18,19]
+      ]    
+
 ############# Utility functions ###################
 def dot(A, B):
     return sum(A[k]*B[k] for k in A if k in B)
@@ -46,6 +65,7 @@ def calc_f(x, i, l, w, e, f, q):
                 calc_f(x, i-1, k, w, e, f, q) + calc_e(x, i, k, l, w, e, q)
                     for k in prev_states])
     return f[i,l]
+
 def calc_b(x, i, r, w, e, b, q):
     if (i, r) not in b:
         if i == len(x)-1:
@@ -83,4 +103,47 @@ def calc_gradient(x, y, w, q):
     # print grad
     # Return the gradient and likelihood
     return (grad, lik)
+
+def train(A, XY, YY):
+    w = [{} for x in range(len(XY))]
+    # w[0] sihong 
+    # w[1] sihong
+    # w[2] sihong
+    # w[3] w[4] huayi 
+    # for 50 iterations
+    g = 3 # group id 
+    w[g] = defaultdict(lambda: 0)
+    
+    for iternum in range(1, 30+1):
+        #print iternum
+        grad = defaultdict(lambda: 0)
+        # Perform regularization
+        reg_lik = 0;
+        for k, v in w[g].items():
+            grad[k] -= 2*v*l2_coeff
+            reg_lik -= v*v*l2_coeff
+        # Get the gradients and likelihoods
+        lik = 0
+        for col in range(0, A.shape[1]):  # col is the index of train data  
+            y = []
+            y.append(0)
+            for yInd in YY[g]:
+                y.append(A[yInd-1,col])
+            y.append(0)
+            #print y;
+            x = [[0]]
+            for yy in YY[g]:
+                x.append(XY[g][yy])
+            x.append([0]);
+            #print x;
+            q = A[:,col]
+            my_grad, my_lik = calc_gradient(x, y, w[g], q)
+            for k, v in my_grad.items(): grad[k] += v
+            lik += my_lik
+        l1 = sum( [abs(k) for k in grad.values()] )
+        print "Iter %r likelihood: lik=%r, reg=%r, reg+lik=%r gradL1=%r" % (iternum, lik, reg_lik, lik+reg_lik, l1)
+        for k, v in grad.items(): w[g][k] += rate * v/l1  
+    print w[g]
  
+ 
+train(A, XY, YY)
--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from numpy import genfromtxt
+from numpy import vstack
+from crf import *
+
+
+# The L2 regularization coefficient and learning rate for SGD
+l2_coeff = 1
+rate = 0.1
+
 
 A = genfromtxt('quiztrain.csv', delimiter=',', skip_header = 0)
 B = genfromtxt('quiztest.csv', delimiter=',', skip_header = 0)
@@ -17,7 +25,7 @@ YY = [
         [13],
         [20],
         [16,15,14],
-        [11,17,18,19],
+        [11,17,18,19]
       ]    
 
 def train(A, XY, YY):
@@ -26,7 +34,50 @@ def train(A, XY, YY):
     # w[1] sihong
     # w[2] sihong
     # w[3] w[4] huayi 
-    return w;
+    # for 50 iterations
+    g = 4 # group id 
+    w[g] = defaultdict(lambda: 0)
+    for iternum in range(1, 30 +1):
+        #print 'iter ', iternum
+        grad = defaultdict(lambda: 0)
+        # Perform regularization
+        reg_lik = 0;
+        for k, v in w[g].items():
+            grad[k] -= 2*v*l2_coeff
+            reg_lik -= v*v*l2_coeff
+        # Get the gradients and likelihoods
+        lik = 0
+        for col in range(0, A.shape[1]):  # col is the index of train data  
+            y = []
+            y.append(0)
+            for yInd in YY[g]:
+                y.append(A[yInd-1,col])
+            y.append(0)
+            #print y;
+            x = [] 
+            x.append([0])
+            for yy in YY[g]:
+                if yy in XY[g]:
+                    x.append(XY[g][yy])
+                else:
+                    x.append([])
+            x.append([0]);
+            #print x;
+            my_grad, my_lik = calc_gradient(x, y, w[g], A[:,col])
+            for k, v in my_grad.items(): grad[k] += v
+            lik += my_lik
+            #print my_lik
+        l1 = sum( [abs(k) for k in grad.values()] )
+        print "Iter %r likelihood: lik=%r, reg=%r, reg+lik=%r gradL1=%r" % (iternum, lik, reg_lik, lik+reg_lik, l1)
+        for k, v in grad.items(): w[g][k] +=  1.0  * v /l1 * rate     
+     
+    import operator
+    sorted_w = sorted(w[g].iteritems(), key= operator.itemgetter(1), reverse = True)
+    for e in sorted_w:
+        print "%20s  %12s" % ( e[0], e[1])
+
+
+
 
 # bx is a test point 
 def test(bx, w, XY, YY):
@@ -37,6 +88,8 @@ def test(bx, w, XY, YY):
     # p[3] p[4] xiaokai
     return p
 
-print A
-print BX
-print BY
+if __name__ == '__main__':
+    #print A
+    #print BX
+    #print BY
+    train(A, XY, YY)
